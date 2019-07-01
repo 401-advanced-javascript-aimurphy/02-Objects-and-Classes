dbe91db3b50732969f509e242f3b4e0ac3e9118c
Aileen Murphy <ai.murf2@gmail.com>
2019-07-01 16:06:04 -0700

    fixed spaces adn smeocolons

--- list/list-constructor.js
+++ list/list-constructor.js
@@ -8,7 +8,7 @@ function List() {
  * Add item to the end of the list
  * @param item
  */
-List.prototype.push = function(item) {
+List.prototype.push = function (item) {
   this.data[this.length] = item;
   this.length++;
 };
@@ -17,63 +17,63 @@ List.prototype.push = function(item) {
  * // Remove an item from the end of the list and return it's value
  * @returns {*}
  */
-List.prototype.pop = function() {
-  let returnValue = this.data[this.length-1];
-  delete this.data[this.length-1];
+List.prototype.pop = function () {
+  let returnValue = this.data[this.length - 1];
+  delete this.data[this.length - 1];
   this.length--;
   return returnValue;
 };
 
 // list.shift 
 // remove an item from the beginning of an array and return its value
-List.prototype.shift = function() {
+List.prototype.shift = function () {
   let returnValue = this.data[0];
-  for(let i=0;i<this.length;i++){
-    this.data[i]=this.data[i+1];
+  for (let i = 0; i < this.length; i++) {
+    this.data[i] = this.data[i + 1];
   }
-  delete this.data[this.length-1];
+  delete this.data[this.length - 1];
   this.length--;
   return returnValue;
 };
 
 // list.unshift
 // adds item(s) to the beginning of an array and returns the new length of the array
-List.prototype.unshift = 
-function(item){
-  for(let i=this.length-1;i>=0;i--){
-    this.data[i+1]=this.data[i];
-  }
-  this.data[0]=item;
-  this.length++;
-  return this.length;
-};
+List.prototype.unshift =
+  function (item) {
+    for (let i = this.length - 1; i >= 0; i--) {
+      this.data[i + 1] = this.data[i];
+    }
+    this.data[0] = item;
+    this.length++;
+    return this.length;
+  };
 
 //list.forEach
 //iterates over an array does not mutate returns nothing.
-List.prototype.forEach = function(callback){
-  for (let i=0;i<this.length;i++){
-    callback(this.data[i],i);
+List.prototype.forEach = function (callback) {
+  for (let i = 0; i < this.length; i++) {
+    callback(this.data[i], i);
   }
 };
 
 //list.map
 //returns a new array with a callback executed on each element
-List.prototype.map = function(callback){
+List.prototype.map = function (callback) {
   let returnValue = new List();
-  for (let i=0;i<this.length;i++){
+  for (let i = 0; i < this.length; i++) {
     console.log(i, this.data[i]);
-    returnValue.push(callback(this.data[i],i));
+    returnValue.push(callback(this.data[i], i));
   }
   return returnValue;
 };
 
 //list.filter
 //returns new array containing previous array elements whose values returned true against filtering criteria 
-List.prototype.map = function(callback){
+List.prototype.map = function (callback) {
   let returnValue = new List();
-  for (let i=0;i<this.length;i++){
+  for (let i = 0; i < this.length; i++) {
     console.log(i, this.data[i]);
-    returnValue.push(callback(this.data[i],i));
+    returnValue.push(callback(this.data[i], i));
   }
   return returnValue;
 };
