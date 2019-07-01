9a80990cbc65ed9c1c9d6cdb3d820fdd9a91c612
Aileen Murphy <ai.murf2@gmail.com>
2019-06-30 17:53:47 -0700

    added configs into subfolders

--- list/index.js
+++ list/index.js
@@ -5,4 +5,14 @@ const List = require('./list-constructor.js');
 let stuff = new List();
 stuff.push('a');
 stuff.push('b');
+stuff.push('c');
+stuff.pop();
+stuff.shift();
+stuff.unshift('d');
+let helloMap = stuff.map(x => 'hello ' + x + '!');
+console.log(helloMap);
+
 let last = stuff.pop();
+
+console.log(last);
+
--- list/list-constructor.js
+++ list/list-constructor.js
@@ -4,7 +4,7 @@ function List() {
   this.length = 0;
   this.data = {};
 }
-/**
+/*
  * Add item to the end of the list
  * @param item
  */
@@ -18,10 +18,49 @@ List.prototype.push = function(item) {
  * @returns {*}
  */
 List.prototype.pop = function() {
-  let returnValue = this.data[this.length];
-  delete this.data[this.length];
+  let returnValue = this.data[this.length-1];
+  delete this.data[this.length-1];
   this.length--;
   return returnValue;
 };
+
+// list.shift 
+// remove an item from the beginning of an array and return its value
+List.prototype.shift = function() {
+  let returnValue = this.data[0];
+  for(let i=0;i<this.length;i++){
+    this.data[i]=this.data[i+1]
+  };
+  delete this.data[this.length-1];
+  this.length--;
+  return returnValue;
+}
+
+// list.unshift
+// adds item(s) to the beginning of an array and returns the new length of the array
+List.prototype.unshift = 
+function(item){
+  for(let i=this.length-1;i>=0;i--){
+    this.data[i+1]=this.data[i];
+  };
+  this.data[0]=item;
+  this.length++;
+  return this.length;
+}
+
+//list.forEach
+//iterates over an array does not mutate returns nothing.
+
+
+//list.map
+//returns a new array with a callback executed on each element
+List.prototype.map = function(callback){
+  let returnValue = new List();
+  for (let i=0;i<this.length;i++){
+    console.log(i, this.data[i]);
+    returnValue.push(callback(this.data[i],i))
+  }
+  return returnValue;
+}
 
 module.exports = List;
